88561529a34b0ab0aed7a17e4bac5b5e62880950
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2026-07-08 22:15:49 +0530

    setup frontend and add isAuthenticated middleware

--- server/src/routes/resume.routes.js
+++ server/src/routes/resume.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import isAuthenticated from "../middleware/isAuthanticated.js";
 import upload from "../middleware/multer.middleware.js";
 import {
   genrateResume,
@@ -7,7 +8,7 @@ import {
 
 const router = express.Router();
 
-router.post("/upload", upload.single("resume"), uploadResume);
-router.get("/generate", genrateResume);
+router.post("/upload", isAuthenticated, upload.single("resume"), uploadResume);
+router.get("/generate", isAuthenticated, genrateResume);
 
 export default router;
